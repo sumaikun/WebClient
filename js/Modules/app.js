45a68f095888f9925d4e6ac1c91c06548103034d
Jesus Vega <ventas.javc@gmail.com>
2016-09-29 08:15:55 -0500

    pagina web iniciada

--- js/Modules/app.js
+++ js/Modules/app.js
@@ -1,1 +1,16 @@
-var app = angular.module("Appi",[]);
+var app = angular.module("Appi",['ngRoute']);
+app.config(function ($routeProvider) { 
+  console.log($routeProvider);
+  $routeProvider    
+    .when('/Main',{ 
+      controller: 'PageController', 
+      templateUrl: 'js/Views/main_page.html' 
+    })
+    .when('/',{ 
+      controller: 'UserController', 
+      templateUrl: 'index.html' 
+    })    
+    .otherwise({
+      redirectTo: '/'
+    });
+});
